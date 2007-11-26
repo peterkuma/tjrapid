9df18cf2a2f893a0f4f4d28a65bb520dfd0f751a
peter <peter@2fdab57a-a2c7-42d6-b491-d82d9bc2802c>
2007-11-26 15:46:55 +0000

    File browser symlink.

--- settings.py
+++ settings.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Django settings for tjrapid project.
 
 DEBUG = True
@@ -13,7 +12,7 @@ MANAGERS = ADMINS
 DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'tjrapid'               # Or path to database file if using sqlite3.
 DATABASE_USER = 'tjrapid'               # Not used with sqlite3.
-DATABASE_PASSWORD = 'abc'               # Not used with sqlite3.
+DATABASE_PASSWORD = 'hopsem'            # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -36,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = 'site_media/'
+MEDIA_ROOT = '/home/peter/www/tjrapid/current/tjrapid/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -68,7 +67,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'tjrapid.urls'
 
 TEMPLATE_DIRS = (
-    '/home/peter/data/www/tjrapid/current/tjrapid/templates',
+    '/home/peter/www/tjrapid/current/tjrapid/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -91,4 +90,3 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.auth',
 )
-
