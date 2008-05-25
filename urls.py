21ef9bae1e371fce6a7c5e30f352043d6589050e
peter <peter@2fdab57a-a2c7-42d6-b491-d82d9bc2802c>
2008-05-25 15:34:23 +0000

    Copyright change.

--- urls.py
+++ urls.py
@@ -13,7 +13,7 @@
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
 #
-# 3. The names of its contributors may be used
+# 3. Neither the name of TJ Rapid nor the names of its contributors may be used
 #    to endorse or promote products derived from this software without
 #    specific prior written permission.
 #
