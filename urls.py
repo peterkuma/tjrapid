fae291640e21b650451ed377d49cd8d7b9e0bf3b
peter <peter@2fdab57a-a2c7-42d6-b491-d82d9bc2802c>
2010-12-17 19:57:11 +0000

    Included RSS feeds in the OB news.

--- urls.py
+++ urls.py
@@ -11,6 +11,8 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
+from news.feed import NewsFeed
+
 urlpatterns = patterns('',
 	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	(r'^admin/eventapp/', include('tjrapid.eventapp.admin_urls')),
@@ -21,6 +23,8 @@ urlpatterns = patterns('',
 	(r'^en/orienteering/competitions/$','tjrapid.ob.views.competitions',
 	    dict(category_name='orienteering')),
 	(r'^(?P<category>[^/]+)/news/',include('tjrapid.news.urls')),
+	(r'^(?P<category>ob)/news/rss/', NewsFeed()),
+	(r'^(?P<category>en)/orienteering/news/rss/', NewsFeed()),
 	(r'^(?P<lang>[^/]+)/(?P<category>[^/]+)/news/',include('tjrapid.news.urls')),
 	(r'^ob/prihlaska/', include('tjrapid.eventapp.urls', namespace='eventapp-sk', app_name='eventapp'), dict(category='ob', namespace='eventapp-sk')),
 	(r'^en/orienteering/entry/', include('tjrapid.eventapp.urls', namespace='eventapp-en', app_name='eventapp'), dict(category='orienteering', namespace='eventapp-en')),
