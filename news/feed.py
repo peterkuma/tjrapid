63bf270dbf44506d5aea70cbae62107f7a10e8e0
peter <peter@2fdab57a-a2c7-42d6-b491-d82d9bc2802c>
2010-12-17 19:55:49 +0000

    Added support for RSS feeds to the news app.

---
+++ news/feed.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# $Id$
+#
+# Copyright (c) 2010 Peter Kuma
+# All rights reserved.
+#
+
+from django.contrib.syndication.views import Feed, FeedDoesNotExist
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from django.utils.translation import ugettext as _
+from django.contrib.markup.templatetags import markup
+
+from models import Article
+from tjrapid.main.models import Category
+
+class NewsFeed(Feed):
+    link = 'ob/news'
+    
+    def get_object(self, request, category):
+        return get_object_or_404(Category, name=category)
+    
+    def title(self, obj):
+        return _('News for %s') % obj.title
+        
+#    def item_description(self, item):
+#        return markup.textile(item.head)
+    
+    def item_author(self, item):
+        return item.author
+    
+    def item_pubdate(self, item):
+        return item.published
+    
+    def items(self, obj):
+        return Article.objects.filter(category=obj)[:5]
