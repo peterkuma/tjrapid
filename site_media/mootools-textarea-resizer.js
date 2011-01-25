2f04131bf180f40c020e67562b916d8bfe6d707e
peter <peter@2fdab57a-a2c7-42d6-b491-d82d9bc2802c>
2011-01-25 22:12:59 +0000

    Added mootools-textarea-resizer.

---
+++ site_media/mootools-textarea-resizer.js
@@ -0,0 +1,102 @@
+/**
+description: Mootools TextareaResizer plugin for Mootools 1.2.x
+
+license: MIT-style license.
+
+copyright: Copyright (c) 2009 Joshua Partogi (http://scrum8.com/).
+
+authors: Joshua Partogi (http://scrum8.com/)
+*/
+var TextareaResizer = new Class({
+
+    initialize: function(element){
+        this.textarea = element;
+        this.element = element;
+    },
+
+    resizable: function(){
+
+        var staticOffset = 0;
+        var iLastMousePos = 0;
+        var iMin = 32;
+
+        var textarea = this.element;
+        textarea.addClass('processed');
+        
+        var div = new Element('div').addClass('resizable-textarea');
+        var span = new Element('span');
+
+        div.wraps(span.wraps(textarea));
+
+        var grippie = new Element('div').addClass('grippie');
+        grippie.inject(span);
+        grippie.setStyle('margin-right', grippie.offsetWidth - textarea.offsetWidth);
+        grippie.setStyle('width', textarea.getStyle('width'));
+
+        var endDrag = function(e) {            
+            document.removeEvent('mousemove', performDrag);
+            document.removeEvent('mouseup', endDrag);
+            
+            //textarea.setStyle('opacity', 1.0);
+            textarea.focus();
+
+            staticOffset = 0;
+            iLastMousePos = 0;
+        }
+
+        var performDrag = function(e) {
+            var iThisMousePos = mousePosition(e).y;
+            var iMousePos = staticOffset + iThisMousePos;
+
+            if (iLastMousePos >= (iThisMousePos)) {
+                iMousePos -= 5;
+            }
+
+            iLastMousePos = iThisMousePos;
+            iMousePos = Math.max(iMin, iMousePos);
+
+            textarea.setStyle('height', iMousePos);
+
+            if (iMousePos < iMin) {
+                endDrag(e);
+            }
+        }
+
+        var startDrag = function(e){
+            textarea.blur();
+
+            iLastMousePos = mousePosition(e).y;
+            staticOffset = textarea.getSize().y - iLastMousePos;
+            //textarea.setStyle('opacity', 0.25);
+            
+            document.addEvent('mousemove', performDrag);
+            document.addEvent('mouseup', endDrag);
+        }
+
+        var mousePosition = function(e) {
+            return {
+                x: e.event.clientX + document.documentElement.scrollLeft,
+                y: e.event.clientY + document.documentElement.scrollTop
+            };
+        }
+
+        grippie.addEvent('mousedown', startDrag);
+    }
+    
+
+});
+
+
+(function(){
+
+    Element.implement({
+
+        resizable: function(){
+            var resizer = new TextareaResizer(this);
+
+            resizer.resizable();
+        }
+        
+    });
+
+})();
