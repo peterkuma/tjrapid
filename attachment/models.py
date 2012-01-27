301ff165fdc5581f2e0918142f64ef1b3c98719b
peter <peter@2fdab57a-a2c7-42d6-b491-d82d9bc2802c>
2012-01-27 09:50:58 +0000

    Bug fix: Attachment.__unicode__() fails if file is not set. Add verbose names to Attachment fields.

--- attachment/models.py
+++ attachment/models.py
@@ -14,16 +14,18 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.generic import GenericForeignKey
 
 class Attachment(Model):
-	content_type = ForeignKey(ContentType)
-	object_id = PositiveIntegerField()
+	content_type = ForeignKey(ContentType, verbose_name=_('content type'))
+	object_id = PositiveIntegerField(_('object ID'))
 	content_object = GenericForeignKey('content_type', 'object_id')
-	file = FileField(upload_to=lambda i, f: "attachment/%s/%s/%s/%s" %
+	file = FileField(_('file'), upload_to=lambda i, f: 'attachment/%s/%s/%s/%s' %
 			 (i.content_type.app_label, i.content_type.model, i.content_object.id, f))
 	
 	def __unicode__(self):
-		return self.file.__unicode__()
+		if self.file:
+			return unicode(os.path.basename(self.file.path))
+		else:
+			return u'Attachment object'
 	
 	class Meta:
 		verbose_name = _('attachment')
 		verbose_name_plural = _('attachments')
-
