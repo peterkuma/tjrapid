00882bb4bb071f6fb26f97f3f1764f10490c8d57
Peter Kuma <peter@peterkuma.net>
2021-07-18 14:11:43 +0200

    ob: Event centre popup

--- lib/templatetags/map.py
+++ lib/templatetags/map.py
@@ -4,7 +4,7 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('map.html')
-def map(lon, lat, points=None, zoom=15, larger_map_link=None):
+def map(lon, lat, points=None, zoom=15, title=None, larger_map_link=None):
     if larger_map_link is None:
         larger_map_link = 'https://www.openstreetmap.org/?mlat={lat}amp;mlon={lon}#map={zoom}/{lat}/{lon}'.format(
             lon=lon,
@@ -15,6 +15,7 @@ def map(lon, lat, points=None, zoom=15, larger_map_link=None):
         'lon': lon,
         'lat': lat,
         'zoom': zoom,
+        'title': title,
         'points': points,
         'larger_map_link': larger_map_link,
     }
