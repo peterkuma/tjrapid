327f2bfd673e78c448500b6d66938a985bd14f88
Peter Kuma <peter@peterkuma.net>
2021-08-13 08:27:36 +0200

    Enable XFrameOptionsMiddleware

--- tjrapid/settings.py
+++ tjrapid/settings.py
@@ -134,7 +134,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'main.middleware.LanguageMiddleware',
 )
 
